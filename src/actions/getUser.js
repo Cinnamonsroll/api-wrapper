90170bc614230e5076737443478f78a47218c61b
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-05-05 21:44:04 -0300

    Update getUser.js

--- src/actions/getUser.js
+++ src/actions/getUser.js
@@ -1,17 +1,5 @@
-const strings = require('../strings.json');
-const fetch = require('node-fetch');
-
+const strings = require('../strings.json'), fetch = require('../../utils/fetch.js');
 const run = (uid, token) => {
-
-    return fetch(`${strings.api.http}/api/u?id=${uid}&sid=${token}`, {
-        method: 'GET'
-    }).then(data => data.json())
-        .then(res => {
-            if(res.code !== 200) return res;
-
-            return res.json;
-        });  
-
+return fetch(`${strings.api.http}/api/u?id=${uid}&sid=${token}`, "GET", {}, true)
 }
-
 module.exports.run = run;
