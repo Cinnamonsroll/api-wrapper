a1a2d223ef1c4b77988d5e68031221513f755e2f
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-05-05 21:42:35 -0300

    Update fetch.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,16 @@
-module.exports = function fetch(url, type, data, json){
-if(!url) throw new Error("Please provide a valid url")
-if(!type || !["get", "post"].includes(type.toLowerCase())) throw new Error("Invalid type only GET and POST request are allowed")
-if(!data || typeof data !== "object") throw new Error("Please include data if no data is required provide an empty object")
-if(!json || ![true, false].includes(json)) throw new Error("Invalid type of json value should be a boolean")
-
+let nodeFetch = require("node-fetch");
+module.exports = function fetch(url, type, data, json) {
+    if (typeof url !== 'string') throw new TypeError('Please provide a valid url');
+    if (typeof type !== 'string' || !['get', 'post'].includes(type.toLowerCase())) throw new TypeError('Invalid type only GET and POST request are allowed');
+    if (!(data instanceof Object)) throw new TypeError('Please include data if no data is required provide an empty object');
+    if (typeof json !== 'boolean') throw new TypeError('Invalid type of json value should be a boolean');
+    return nodeFetch(url, {
+            method: type,
+            ...data
+        }).then(data => data.json())
+        .then(res => {
+            if (res.code !== 200) return res
+            return json === true ? res.json : res
+        });
+}
 }
